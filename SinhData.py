5324a0a2b3597cb0debe1f5dc5a22009bd4b3459
cownon <duongnkt.t2.1922@gmail.com>
2024-10-03 13:44:01 +0700

    Change data.npz

--- SinhData.py
+++ SinhData.py
@@ -5,9 +5,9 @@ base  = 3
 side  = base*base
 squares = side*side
 empties = squares * 5//9
+N = 9
 
 data = []
-test = []
 
 # pattern for a baseline valid solution
 def pattern(r,c): 
@@ -17,7 +17,43 @@ def pattern(r,c):
 def shuffle(s): 
     return sample(s, len(s)) 
 
-for i in range(1, 10000):
+def isSafe(aa, row, col, num):
+    for x in range(9):
+        if aa[row][x] == num:
+            return False
+
+    for x in range(9):
+        if aa[x][col] == num:
+            return False
+
+    startRow = row - row % 3
+    startCol = col - col % 3
+    for i in range(3):
+        for j in range(3):
+            if aa[i + startRow][j + startCol] == num:
+                return False
+    return True
+
+def solveSudoku(aa, row, col):
+    if (row == N - 1 and col == N):
+        return True
+      
+    if col == N:
+        row += 1
+        col = 0
+
+    if aa[row][col] > 0:
+        return solveSudoku(aa, row, col + 1)
+    for num in range(1, N + 1, 1):
+        if isSafe(aa, row, col, num):         
+            aa[row][col] = num
+            if solveSudoku(aa, row, col + 1):
+                return True
+
+        aa[row][col] = 0
+    return False
+
+for i in range(0, 5000):
     rBase = range(base) 
     rows  = [ g*base + r for g in shuffle(rBase) for r in shuffle(rBase) ] 
     cols  = [ g*base + c for g in shuffle(rBase) for c in shuffle(rBase) ]
@@ -30,21 +66,11 @@ for i in range(1, 10000):
     for line in board: 
         tmp.append(line)
 
-    data.append(tmp)
-
-    tmp_test = []
-    for p in sample(range(squares), empties):
-        board[p//side][p%side] = 0
-
-    numSize = len(str(side))
-    for line in board:
-        tmp_test.append([int(n) if n else 0 for n in line])
-
-    test.append(tmp_test)
-
+    if (solveSudoku(tmp, 0, 0)):
+        data.append(tmp)
+    else:
+        print("No")
 
 data_np = np.array(data)
 np.savez_compressed('D:/02_Tùng Dương_12T2/python code/ProjectPythonStudy/data.npz', data = data_np)
-
-test_np = np.array(test)
-np.savez_compressed('D:/02_Tùng Dương_12T2/python code/ProjectPythonStudy/test.npz', test = test_np)
+print(len(data))
